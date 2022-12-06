42a1725d8716d9611278ef8ab673fb5b505c2f6c
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-12-06 20:59:43 +0300

    feat: add func to get location coordinates

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,25 +1,37 @@
-import axios from "axios";
+import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_BASE_API_URL;
-const appid = process.env.REACT_APP_API_KEY;
+const baseURL = process.env.REACT_APP_BASE_API_URL
+const appid = process.env.REACT_APP_API_KEY
+const geoCodeUrl = process.env.REACT_APP_GEOCODE_API_URL || ''
 
 export const baseRequest = axios.create({
   baseURL,
   timeout: 2500,
   params: { appid },
-});
+})
+
+export async function getLocationCoordinates(location: string) {
+  try {
+    const { data } = await baseRequest.get(geoCodeUrl, {
+      params: { q: location, limit: 1 },
+    })
+    return data
+  } catch (error: any) {
+    throw error
+  }
+}
 
 export function handleRequestError(error: any) {
-  console.group("Request error");
+  console.group('Request error')
   if (error.response) {
-    console.log("data:", error.response.data);
-    console.log("status:", error.response.status);
-    console.log("headers:", error.response.headers);
+    console.log('data:', error.response.data)
+    console.log('status:', error.response.status)
+    console.log('headers:', error.response.headers)
   } else if (error.request) {
-    console.log("request:", error.request);
+    console.log('request:', error.request)
   } else {
-    console.log("message:", error.message);
+    console.log('message:', error.message)
   }
-  console.log("config:", error.config);
-  console.groupEnd();
+  console.log('config:', error.config)
+  console.groupEnd()
 }
