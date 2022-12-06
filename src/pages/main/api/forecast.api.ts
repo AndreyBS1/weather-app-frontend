9aee1aa5fae76b59ecd45e880b45c9093502cd3c
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-12-06 21:35:14 +0300

    feat: add forecast api

---
+++ src/pages/main/api/forecast.api.ts
@@ -0,0 +1,24 @@
+import { api } from 'src/shared/api'
+
+const weatherUrl = process.env.REACT_APP_WEATHER_API_URL || ''
+
+export async function getLocationForecast(location: string) {
+  try {
+    const locationGeoData = await api.getLocationGeoData(location)
+    const { lat, lon } = locationGeoData
+
+    const { data } = await api.baseRequest.get(`${weatherUrl}/forecast`, {
+      params: { lat, lon },
+    })
+
+    const currentHour = new Date().getHours()
+    const acceptedHour = Math.round(currentHour / 3) * 3
+
+    const forecastByCurrentHour = data.list.filter((forecastData: any) =>
+      forecastData.dt_txt.includes(`${acceptedHour}:00:00`)
+    )
+    return forecastByCurrentHour
+  } catch (error) {
+    api.handleRequestError(error)
+  }
+}
--- src/shared/api.ts
+++ src/shared/api.ts
@@ -4,13 +4,13 @@ const baseURL = process.env.REACT_APP_BASE_API_URL
 const appid = process.env.REACT_APP_API_KEY
 const geoCodeUrl = process.env.REACT_APP_GEOCODE_API_URL || ''
 
-export const baseRequest = axios.create({
+const baseRequest = axios.create({
   baseURL,
   timeout: 2500,
   params: { appid },
 })
 
-export async function getLocationCoordinates(location: string) {
+async function getLocationGeoData(location: string) {
   try {
     const { data } = await baseRequest.get(geoCodeUrl, {
       params: { q: location, limit: 1 },
@@ -21,7 +21,7 @@ export async function getLocationCoordinates(location: string) {
   }
 }
 
-export function handleRequestError(error: any) {
+function handleRequestError(error: any) {
   console.group('Request error')
   if (error.response) {
     console.log('data:', error.response.data)
@@ -35,3 +35,5 @@ export function handleRequestError(error: any) {
   console.log('config:', error.config)
   console.groupEnd()
 }
+
+export const api = { baseRequest, getLocationGeoData, handleRequestError }
