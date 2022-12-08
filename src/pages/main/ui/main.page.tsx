326e6690dcb1e663cc91d872333a6e63d83c6bc1
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-12-08 21:20:26 +0300

    feat: update main page logic

--- src/pages/main/ui/main.page.tsx
+++ src/pages/main/ui/main.page.tsx
@@ -1,17 +1,47 @@
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
-import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
+import { ChangeEvent, FocusEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { getLocationForecast } from '../api'
-
-const weather: any[] = []
+const locationRegEx =
+  /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/
+const errorMessage = 'Invalid location name!'
 
 export function Page() {
+  const navigate = useNavigate()
+
+  const [query, setQuery] = useState('')
+  const [isError, setIsError] = useState(false)
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value } = event.target
+    setQuery(value)
+    if (isError) {
+      setIsError(false)
+    }
+  }
+
+  const handleBlur = (
+    event: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
+  ) => {
+    const { value } = event.target
+    if (!locationRegEx.test(value)) {
+      setIsError(true)
+    }
+  }
+
   const handleSubmit = async (event: any) => {
     event.preventDefault()
-    const forecast = await getLocationForecast('Москва')
-    console.log('forecast:', forecast)
+    if (!locationRegEx.test(query)) {
+      setIsError(true)
+      return
+    }
+    if (!isError) {
+      navigate(`/${query}`)
+    }
   }
 
   return (
@@ -19,20 +49,15 @@ export function Page() {
       <Container>
         <Box component={'form'} onSubmit={handleSubmit}>
           <TextField
-            label="Enter a city"
-            placeholder="Enter a city to check the weather"
+            type="search"
+            label="Enter a city to check the weather"
+            autoComplete="off"
             fullWidth
+            error={isError}
+            helperText={isError ? errorMessage : ''}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
-        </Box>
-
-        <Box>
-          {weather.length ? (
-            <>
-              {weather.map((day) => {
-                ;<Paper>{day}</Paper>
-              })}
-            </>
-          ) : null}
         </Box>
       </Container>
     </main>
