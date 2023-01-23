31f146a442148b1a3269b60d8115bfa6cf0d3fa5
AndreyBS1 <andrey.babenkov.00@gmail.com>
2023-01-23 17:59:09 +0300

    feat: add forecast page components

---
+++ src/pages/location/ui/location.page.tsx
@@ -0,0 +1,12 @@
+export function Page() {
+  return (
+    <main>
+      <div>
+        <h1>Location name</h1>
+        <h2>Current temperature</h2>
+      </div>
+      <div>5 day forecast</div>
+      <div>Details</div>
+    </main>
+  )
+}
