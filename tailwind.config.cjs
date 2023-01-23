432fd3ccdcccf96be7299aea11869d4a81b07348
AndreyBS1 <andrey.babenkov.00@gmail.com>
2023-01-23 20:04:14 +0300

    feat: add background colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azur: '#007fff',
+        indigo: '#4b0082',
+      },
+    },
   },
   plugins: [],
 }
